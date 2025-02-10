f00ab4b8e34c1bd343d8b02484678e2f9ce9fa14
Arnaud875 <arnaud.nickles33@gmail.com>
2025-02-10 14:25:51 +0100

    [Refactor] Retrieving the latest dev/server changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,7 @@
 #include "Database/Database.hpp"
 #include "Utils/Logger/Logger.hpp"
-#include "Api/RouteManager.hpp"
-#include <crow.h>
 
 int main() {
-    Utils::Logger::Logger::SafeSingletonCreate("Logger");
-    Database::Database &database = Database::Database::SafeSingletonCreate("Database");
 
-    crow::SimpleApp app;
-    if constexpr (DDEBUG) {
-        app.loglevel(crow::LogLevel::DEBUG);
-    }
-
-    try {
-        database.Connect(DATABASE_FILE_NAME);
-
-        Api::RouteManager &routeManager = Api::RouteManager::CreateInstance(app);
-        routeManager.SetupRoutes();
-    } catch (const std::exception &e) {
-        if (database.IsConnected()) {
-            database.Close();
-        }
-
-        return 1;
-    }
-
-    app.port(8080).run();
-
-    database.Close();
     return 0;
 }
